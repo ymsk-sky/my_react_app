0f99c8cb5ca4c4f2c806786a91b8713ea831d538
ymsk-sky <ymsk.sky.95@gmail.com>
2024-06-17 22:09:59 +0900

    comple myValueLimited

--- src/components/MyTextField.tsx
+++ src/components/MyTextField.tsx
@@ -8,8 +8,20 @@ export default function MyTextField() {
     const [myString, setMyString] = useState<string>("");
 
     const updataMyValueLimited = (e: React.ChangeEvent<HTMLInputElement>) => {
-        let val = e.target.value;
-        setMyValue(val);
+        /* 例) 入力: 00.123.45 -> 0.12345 になる */
+        const value = e.target.value;
+        // 数字と小数点以外を削除
+        const numericValue = value.replace(/[^0-9.]/g, "");
+        // 1つめの小数点位置を取得
+        const dotIndex = numericValue.indexOf(".");
+        let limitedValue = numericValue;
+        if (dotIndex >= 0) {
+            // ピリオドが存在する場合, 1つ目以外は削除
+            limitedValue = numericValue.slice(0, dotIndex + 1) + numericValue.slice(dotIndex + 1).replace(/\./g, "");
+        }
+        // 文字列先頭が複数の0の場合1つにする
+        limitedValue = limitedValue.replace(/^0+([0-9])/, `$1`);
+        setMyValue(limitedValue);
     };
 
     const updateMyStringLimited = (e: React.ChangeEvent<HTMLInputElement>) => {
